22ffed7d57f99ddd8d5ea3053ba9e7bbb1847964
Tavisca-princekumar-prajapati <118422030+Tavisca-princekumar-prajapati@users.noreply.github.com>
2025-06-12 14:54:06 +0530

    controller

---
+++ JPMC.Hackathon.RapMentor/src/JPMC.Hackathon.RapMentor/Controllers/QnAController.cs
@@ -0,0 +1,35 @@
+﻿using JPMC.Hackathon.RapMentor.Contract.Interfaces;
+using JPMC.Hackathon.RapMentor.Contract.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JPMC.Hackathon.RapMentor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QnAController : ControllerBase
+    {
+        private readonly IQnAService _qnAService;
+
+        public QnAController(IQnAService qnAService)
+        {
+            _qnAService = qnAService;
+        }
+
+        // GET: api/<QnAController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<QnAController>
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            return await _qnAService.GetAsync(value);
+        }
+
+    }
+}
