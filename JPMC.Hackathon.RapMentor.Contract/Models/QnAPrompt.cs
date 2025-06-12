6356630851017b097ced4c915bcf4ea739d95bba
Tavisca-princekumar-prajapati <118422030+Tavisca-princekumar-prajapati@users.noreply.github.com>
2025-06-12 14:30:57 +0530

    added qna controller

---
+++ JPMC.Hackathon.RapMentor.Contract/Models/QnAPrompt.cs
@@ -0,0 +1,12 @@
+﻿namespace JPMC.Hackathon.RapMentor.Contract.Models
+{
+    public class QnAPrompt
+    {
+        public List<Prompt> Prompts { get; set; }
+    }
+    public class Prompt
+    {
+        public string Role { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ JPMC.Hackathon.RapMentor.Services/Services/QnAService.cs
@@ -0,0 +1,13 @@
+﻿using JPMC.Hackathon.RapMentor.Contract.Interfaces;
+using JPMC.Hackathon.RapMentor.Services.Adapters;
+
+namespace JPMC.Hackathon.RapMentor.Services.Services
+{
+    public class QnAService : IQnAService
+    {
+        public async Task<string> GetAsync(string input)
+        {
+            return await BedrockKBAdapter.GetAsync(input);
+        }
+    }
+}
